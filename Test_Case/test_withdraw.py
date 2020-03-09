d59a8fab087346e11625026019553fc00df0e706
周振全 <17764509133@163.com>
2020-03-09 21:29:16 +0800

    完善取现的测试用例

--- Test_Case/test_withdraw.py
+++ Test_Case/test_withdraw.py
@@ -16,6 +16,8 @@ from Common.config import conf
 from Common.request_1 import SendRequests
 from Common.Login import log
 from Common.Colour import color
+from Common.Connect_DB import DB
+from decimal import Decimal
 
 file_path = os.path.join(DATADIR, "apicases.xlsx")
 
@@ -25,6 +27,7 @@ class Test_Withdraw(unittest.TestCase):
     excel = operation_excel(file_path, 'withdraw')
     cases = excel.read_excel()
     request = SendRequests()
+    db = DB()
 
     @data(*cases)
     def test_withdraw(self, case):
@@ -43,21 +46,34 @@ class Test_Withdraw(unittest.TestCase):
         method = case['method']
         row = case["case_id"] + 1
 
+        # 判断是否需要进行sql检验
+        if case["check_sql"] :
+            sql = case["check_sql"].format(conf.get("test_data","phone"))
+            start_money = self.db.find_one(sql)["leave_amount"]
 
         response = self.request.send_requests_sc(url=url, method=method, json=data, headers=headers)
         res = response.json()
 
         # 是否是登录接口 如果是就获取token
         if case['interface'].lower() == 'login':
+            # 提取用户ID保存为类属性
             Test_Withdraw.member_id = jsonpath.jsonpath(res, '$..id')[0]
             token = jsonpath.jsonpath(res, '$..token')[0]
             token_type = jsonpath.jsonpath(res, '$..token_type')[0]
+            # 提取token保存类属性
             Test_Withdraw.token_value = token_type + " " + token
 
-        # 断言比对预期结果和实际结果
+        # 断言  比对预期结果和实际结果
         try:
             self.assertEqual(expected['code'], res['code'])
             self.assertEqual(expected['msg'], res['msg'])
+            # 判断是否需要进行sql检验
+            if case["check_sql"]:
+                sql = case["check_sql"].format(conf.get("test_data","phone"))
+                end_money = self.db.find_one(sql)["leave_amount"]
+                # 比对取现金额是否正确
+                self.assertEqual(Decimal(str(data["amount"])),start_money-end_money)
+
         except AssertionError as e:
             print("预期结果：",expected)
             print("实际结果：",res)
