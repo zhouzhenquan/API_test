301a23121bbdf6f03f0120a37365e404253a0ed1
zhouzhenquan <17764509133@163.com>
2020-08-04 21:08:10 +0800

    更换测试报告

--- Run.py
+++ Run.py
@@ -10,6 +10,7 @@ import unittest
 from Library.HTMLTestRunnerNew import HTMLTestRunner
 from Common.Path import CASEDIR, REPORTDIR
 from Common.filetime import File_time
+from BeautifulReport import BeautifulReport
 from Test_Case import test_Main
 from Common.Email import send_email
 
@@ -18,12 +19,13 @@ loader = unittest.TestLoader()
 suite.addTest(loader.discover(CASEDIR))
 
 # suite = unittest.defaultTestLoader.loadTestsFromModule(test_Main)
+# file_path = os.path.join(REPORTDIR, File_time.time() + 'report.html')
+# runner = HTMLTestRunner(stream=open(file_path, 'wb'),
+#                         title="Python报告",
+#                         description="登录测试",
+#                         tester="周振全")
+# runner.run(suite)
+# send_email(file_path,"Python_24报告")
 
-file_path = os.path.join(REPORTDIR, File_time.time() + 'report.html')
-
-runner = HTMLTestRunner(stream=open(file_path, 'wb'),
-                        title="Python报告",
-                        description="登录测试",
-                        tester="周振全")
-runner.run(suite)
-send_email(file_path,"Python_24报告")
+br = BeautifulReport(suite)
+br.report("前程贷用例",filename=File_time.time() + 'report.html',report_dir=REPORTDIR)
